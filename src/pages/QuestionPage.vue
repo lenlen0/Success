feb31cb1431f118683267552135ab803dec2e131
firephoton <theogez1210@gmail.com>
2025-10-16 15:14:12 +0200

    initialisation de la branche

--- src/pages/QuestionPage.vue
+++ src/pages/QuestionPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="flex flex-center">
     <h1>QUESTION PAGE</h1>
-    <h2>C'est beau la technologie</h2>
+    <h2>Purée ! C'est beau la technologie</h2>
   </q-page>
 </template>
 
