926e149ce96ae1295c3bb42e4438daeeb444beb0
RomainZekh <romainzekh@gmail.com>
2025-11-17 12:28:53 -0500

    Correction CORS api.php

--- backend/api.php
+++ backend/api.php
@@ -15,8 +15,13 @@ $User = new User();
 // En-t  tes pour le JSON et CORS
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(200);
+    exit;
+}
 
 // R  pertoire o   sont stock  s les fichiers JSON
 //$basePath = __DIR__ . '/data/';
