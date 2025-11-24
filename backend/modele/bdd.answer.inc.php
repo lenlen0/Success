8d70bcd5c888d2db357ee9351afeec003049d6bd
RomainZekh <romainzekh@gmail.com>
2025-11-24 11:34:50 -0500

    #64

---
+++ backend/modele/bdd.answer.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once "bdd.inc.php";
+
+class Answer extends ConnexionPDO {
+
+    public function addAnswer($name, $isCorrect, $idQuestion) {
+        try {
+            $req = $this->conn->prepare("INSERT INTO Answer (name, isCorrect, idQuestion)
+                VALUES (:name, :isCorrect, :idQuestion)");
+            $req->bindValue(':name', $name, PDO::PARAM_STR);
+            $req->bindValue(':isCorrect', $status, PDO::PARAM_INT);
+            $req->bindValue(':idQuestion', $code, PDO::PARAM_INT);
+            $resultat = $req->execute();
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+        return $resultat;
+    }
+
+    public function getAnswerByIDQuestion($idQuestion) {
+        $resultat = array();
+        try {
+            $req = $this->conn->prepare("SELECT * FROM Answer WHERE idQuestion = :idQuestion");
+            $req->bindValue(':idQuestion', $idQuestion, PDO::PARAM_INT);
+            $req->execute();
+
+            $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+        return $resultat;
+    }
+}
+
+?>
