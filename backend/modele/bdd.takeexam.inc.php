122da5c235d9b89ce80a25b037ba671709f138e3
RomainZekh <romainzekh@gmail.com>
2025-12-04 10:23:12 -0500

    #84

--- backend/modele/bdd.takeexam.inc.php
+++ backend/modele/bdd.takeexam.inc.php
@@ -6,12 +6,13 @@ class TakeExam extends ConnexionPDO {
 
     public function userTakeExam($id_s11, $idExam, $answer, $grade) {
         try {
+            $answer_json = json_encode($answer);
 
             $req = $this->conn->prepare("INSERT INTO TakeExam (id_s11, idExam, answer, grade)
                 VALUES (:id_s11, :idExam, :answer, :grade)");
             $req->bindValue(':id_s11', $id_s11, PDO::PARAM_INT);
             $req->bindValue(':idExam', $idExam, PDO::PARAM_INT);
-            $req->bindValue(':answer', $answer, PDO::PARAM_STR);
+            $req->bindValue(':answer', $answer_json, PDO::PARAM_STR);
             $req->bindValue(':grade', $grade, PDO::PARAM_STR);
             $resultat = $req->execute();
         } catch (PDOException $e) {
