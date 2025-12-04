02b8615df4f454f2ef13d5e5696bffdfda17b053
RomainZekh <romainzekh@gmail.com>
2025-12-04 05:34:19 -0500

    #80

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -81,7 +81,7 @@ const linksList = [
     title: 'Déconnexion',
     caption: 'Quitter vers le portail de connexion',
     icon: 'logout',
-    link: 'https://www.urssaf.fr/accueil.html'
+    link: 'http://localhost:9000/#/'
   }
 ]
 
