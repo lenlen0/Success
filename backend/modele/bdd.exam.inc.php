ad0e79e2e06a45621c0674b1030e8966da9f53c8
RomainZekh <romainzekh@gmail.com>
2025-12-10 05:13:57 -0500

    Fini #108

--- backend/modele/bdd.exam.inc.php
+++ backend/modele/bdd.exam.inc.php
@@ -9,14 +9,20 @@ class Exam extends ConnexionPDO {
         try {
             $this->conn->exec("SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''))");
 
-            $req = $this->conn->prepare("SELECT e.idExam, e.name AS exam_name, e.dateExam, e.status, e.code, QU.name AS quizz_name, QU.idQuizz, g.idGroup, g.name AS group_name, ROUND(AVG(te.grade), 2) AS avg_grade
-                FROM Exam e INNER JOIN Quizz QU ON e.idQuizz = QU.idQuizz INNER JOIN `Group` g ON e.idGroup = g.idGroup
+            $req = $this->conn->prepare("SELECT e.idExam, e.name AS exam_name, e.dateExam, e.status, e.code,
+                    QU.name AS quizz_name, QU.idQuizz,
+                    g.idGroup, g.name AS group_name,
+                    ROUND(AVG(te.grade), 2) AS avg_grade
+                FROM Exam e
+                INNER JOIN Quizz QU ON e.idQuizz = QU.idQuizz
+                INNER JOIN `Group` g ON e.idGroup = g.idGroup
                 LEFT JOIN TakeExam te ON e.idExam = te.idExam
+                WHERE e.idExam = :idExam
                 GROUP BY e.idExam, e.name, e.dateExam, e.status, e.code, QU.name, QU.idQuizz, g.idGroup, g.name
-                ORDER BY e.idExam WHERE e.idExam = :idExam");
+                ORDER BY e.idExam
+            ");
             $req->bindValue(':idExam', $idExam, PDO::PARAM_INT);
             $req->execute();
-
             $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
         } catch (PDOException $e) {
             die($e->getMessage());
