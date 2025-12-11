063d3b8a35d1fe7246097092e8b553aeb5cf32a5
RomainZekh <romainzekh@gmail.com>
2025-12-11 02:47:38 -0500

    Fix #102

--- src/pages/ExamU.vue
+++ src/pages/ExamU.vue
@@ -77,7 +77,9 @@
 <script setup>
 import { ref } from 'vue'
 import { onMounted } from 'vue'
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const rows = ref([])
 const questions = ref([])
 const answers = ref({})
